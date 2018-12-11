c6eca1e70bc75f93e966c4156caf5874e267cd3f
unknown <johnmasiello@github.com>
2018-12-11 14:42:07 -0600

    2nd commit

--- src/pizzashop/App.java
+++ src/pizzashop/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         int size = 12;
         String crust = "Thin";
-        String[] toppings = {"Cheese", "Tomato Sauce", "Pepperoni"};
+        //String[] toppings = {"Cheese", "Tomato Sauce", "Pepperoni"};
 
         Pizza cheesePizza; // only in stack
         cheesePizza = new Pizza(size, crust, "Cheese", "Tomato Sauce", "Pepperoni"); // now in heap
